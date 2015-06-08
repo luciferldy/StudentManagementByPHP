ab53966c0a461db3b59cabc19d93cf843d01cbd1
MaybeMercy <2460572865@qq.com>
2015-06-08 15:58:28 +0800

    upload image

---
+++ query_stu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>管理员查找学生</title>
+</head>
+<body>
+<h2>管理员查找学生</h2>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maybe霏
+ * Date: 2015/6/8
+ * Time: 9:06
+ */
+?>
